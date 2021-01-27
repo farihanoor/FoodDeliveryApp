11fdae01d2b615f159982c9c01b46d084fadb3c9
Fariha Noor <fariha.git@gmail.com>
2021-01-28 05:01:24 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.fooddeliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+
+
+
+    }
+}
